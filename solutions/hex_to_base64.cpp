1c7b4763259ccddfa7025ae6cdf463fe72c2729f
Luuls <luan13467@hotmail.com>
2023-09-17 19:08:42 -0300

    refatorei o código

--- solutions/hex_to_base64.cpp
+++ solutions/hex_to_base64.cpp
@@ -13,8 +13,8 @@ std::string hexToBase64(const std::string& hexString);
 int main(int argc, const char* argv[]) {
     std::string input;
     if (argc < 2) {
-        input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         std::cout << "You didn't pass any hex string as argument.\nBy default, this program will use the following string as input:\n";
+        input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         std::cout << input;
         std::cout << "\n\nbase64 result:\n";
     }
@@ -26,7 +26,6 @@ int main(int argc, const char* argv[]) {
         input = argv[1];
     }
 
-
     std::cout << hexToBase64(input) << '\n';
 
     return 0;
@@ -37,20 +36,17 @@ std::string hexToBase64(const std::string& hexString) {
     using std::vector;
 
     string binString = hexToBin(hexString);
-
     binString = padStringWithChar(binString, '0', 6, true);
 
-    vector<string> sixBitBytes(std::ceil((float)binString.length() / 6));
-
+    vector<string> sixBitBlocks(std::ceil((float)binString.length() / 6));
     for (size_t i = 0; i < binString.length(); i += 6) {
-        sixBitBytes[i / 6] = binString.substr(i, 6);
+        sixBitBlocks[i / 6] = binString.substr(i, 6);
     }
 
     string base64Symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     string base64String = "";
-
-    for (auto code : sixBitBytes) {
-        base64String += base64Symbols[binToDecimal(code)];
+    for (auto block : sixBitBlocks) {
+        base64String += base64Symbols[binToDecimal(block)];
     }
 
     base64String = padStringWithChar(base64String, '=', 4, true);
